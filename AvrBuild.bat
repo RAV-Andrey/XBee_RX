0b420e4bda93c2c6a8e5bc396c9216305fac3558
RAV-Andrey <rav107@yandex.ru>
2013-10-18 21:30:43 +0600

    Delete AvrBuild.bat

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\test_jnag\labels.tmp" -fI -W+ie -C V2E -o "C:\test_jnag\test_jnag.hex" -d "C:\test_jnag\test_jnag.obj" -e "C:\test_jnag\test_jnag.eep" -m "C:\test_jnag\test_jnag.map" "C:\test_jnag\main.asm"
